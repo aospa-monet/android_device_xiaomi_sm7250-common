04d3ae0f75ae9bc0b76d650978145fd5d2dd7992
Yumi Yukimura <me.cafebabe@gmail.com>
2024-06-12 22:41:36 +0800

    sm7250-common: Setup DeviceAsWebcam
    Change-Id: Icf8e10cd21c4eef9a66af624971ffe9c4d39d992

--- lito.mk
+++ lito.mk
@@ -410,6 +410,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/CarrierConfig
 
 PRODUCT_PACKAGES += \
+    DeviceAsWebcamOverlay \
     LitoNfcResCommon \
     SettingsLibOverlayCommon
 # Partitions
