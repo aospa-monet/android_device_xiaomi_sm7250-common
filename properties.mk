60a42b8f7682daea009ad6a3c282d722bc5927d7
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-08-04 00:34:12 +0000

    sm7250-common: Use lmk kill_timeout_ms value from CLO
    [1] - Qualcomm has its own adaptation, via ro.lmk.kill_timeout_ms_dup. In this case we will just use the same value
    
    Change-Id: I350a0bd2502fb3e42db606d51b513137bc9a8509
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>

--- properties.mk
+++ properties.mk
@@ -374,7 +374,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 ro.config.low_ram=false \
 ro.lmk.use_minfree_levels=true \
 ro.lmk.kill_heaviest_task=true \
-ro.lmk.kill_timeout_ms=15 \
+ro.lmk.kill_timeout_ms=100 \
 ro.lmk.log_stats=true
 
 # Media
